598fa1a959874d8ae9c18c6ef30726e0b48ec858
Wey2shop <51450618+Wey2shop@users.noreply.github.com>
2021-07-27 23:19:11 +0100

    Added Aluminium.

---
+++ src/main/java/net/mcreator/cm/block/Aluminium_OreBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.cm.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.cm.CmModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CmModElements.ModElement.Tag
+public class Aluminium_OreBlockBlock extends CmModElements.ModElement {
+	@ObjectHolder("cm:aluminium_ore_block")
+	public static final Block block = null;
+	public Aluminium_OreBlockBlock(CmModElements instance) {
+		super(instance, 19);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("aluminium_ore_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
