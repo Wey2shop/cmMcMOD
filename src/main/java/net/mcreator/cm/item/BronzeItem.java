5e53b96b3621d0d199e4d20269111c45c364ec69
Wey2shop <51450618+Wey2shop@users.noreply.github.com>
2021-07-28 00:04:46 +0100

    Added Bronze

---
+++ src/main/java/net/mcreator/cm/item/BronzeItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.cm.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.cm.CmModElements;
+
+@CmModElements.ModElement.Tag
+public class BronzeItem extends CmModElements.ModElement {
+	@ObjectHolder("cm:bronze")
+	public static final Item block = null;
+	public BronzeItem(CmModElements instance) {
+		super(instance, 44);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("bronze");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
