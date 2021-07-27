d3bf7c8e8f8bdff21ce18b0a810e71de94b7515d
Wey2shop <51450618+Wey2shop@users.noreply.github.com>
2021-07-27 23:44:47 +0100

    Added Tin

---
+++ src/main/java/net/mcreator/cm/item/Tin_OreIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.cm.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.cm.CmModElements;
+
+@CmModElements.ModElement.Tag
+public class Tin_OreIngotItem extends CmModElements.ModElement {
+	@ObjectHolder("cm:tin_ore_ingot")
+	public static final Item block = null;
+	public Tin_OreIngotItem(CmModElements instance) {
+		super(instance, 23);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("tin_ore_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
