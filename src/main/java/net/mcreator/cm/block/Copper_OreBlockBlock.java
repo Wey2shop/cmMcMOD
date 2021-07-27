ce10924fd6dbc2ccb1a85fa0224aa81c9d6c4974
Wey2shop <51450618+Wey2shop@users.noreply.github.com>
2021-07-27 21:46:41 +0100

    Added Copper ore

---
+++ src/main/java/net/mcreator/cm/block/Copper_OreBlockBlock.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.cm.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.cm.CmModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CmModElements.ModElement.Tag
+public class Copper_OreBlockBlock extends CmModElements.ModElement {
+	@ObjectHolder("cm:copper_ore_block")
+	public static final Block block = null;
+	public Copper_OreBlockBlock(CmModElements instance) {
+		super(instance, 13);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(5f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("copper_ore_block");
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/cm/item/Copper_OreIngotItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.cm.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.cm.CmModElements;
+
+@CmModElements.ModElement.Tag
+public class Copper_OreIngotItem extends CmModElements.ModElement {
+	@ObjectHolder("cm:copper_ore_ingot")
+	public static final Item block = null;
+	public Copper_OreIngotItem(CmModElements instance) {
+		super(instance, 11);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64).rarity(Rarity.COMMON));
+			setRegistryName("copper_ore_ingot");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
