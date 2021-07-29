604f333d42688460aefb6503114b9bfe5271548c
Wey2shop <51450618+Wey2shop@users.noreply.github.com>
2021-07-29 23:56:31 +0100

    Added MachineBlocks. Base, Lid & Press. Also Added Press Animation

---
+++ src/main/java/net/mcreator/cm/block/MachinePressAnimation3Block.java
@@ -0,0 +1,76 @@
+
+package net.mcreator.cm.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.cm.CmModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@CmModElements.ModElement.Tag
+public class MachinePressAnimation3Block extends CmModElements.ModElement {
+	@ObjectHolder("cm:machine_press_animation_3")
+	public static final Block block = null;
+	public MachinePressAnimation3Block(CmModElements instance) {
+		super(instance, 72);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items.add(() -> new BlockItem(block, new Item.Properties().group(null)).setRegistryName(block.getRegistryName()));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void clientLoad(FMLClientSetupEvent event) {
+		RenderTypeLookup.setRenderLayer(block, RenderType.getCutout());
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.GROUND).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).notSolid()
+					.setOpaque((bs, br, bp) -> false));
+			setRegistryName("machine_press_animation_3");
+		}
+
+		@Override
+		public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) {
+			return true;
+		}
+
+		@Override
+		public VoxelShape getShape(BlockState state, IBlockReader world, BlockPos pos, ISelectionContext context) {
+			Vector3d offset = state.getOffset(world, pos);
+			return VoxelShapes.or(makeCuboidShape(0, -4, 0, 16, 16, 16)).withOffset(offset.x, offset.y, offset.z);
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/cm/procedures/MachineBlockAnimationSCRIPTProcedure.java
@@ -0,0 +1,373 @@
+package net.mcreator.cm.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.state.Property;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.cm.block.MachinePressBlock;
+import net.mcreator.cm.block.MachinePressAnimation3Block;
+import net.mcreator.cm.block.MachinePressAnimation2Block;
+import net.mcreator.cm.block.MachinePressAnimation1Block;
+import net.mcreator.cm.CmModElements;
+import net.mcreator.cm.CmMod;
+
+import java.util.Map;
+
+@CmModElements.ModElement.Tag
+public class MachineBlockAnimationSCRIPTProcedure extends CmModElements.ModElement {
+	public MachineBlockAnimationSCRIPTProcedure(CmModElements instance) {
+		super(instance, 73);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				CmMod.LOGGER.warn("Failed to load dependency entity for procedure MachineBlockAnimationSCRIPT!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				CmMod.LOGGER.warn("Failed to load dependency x for procedure MachineBlockAnimationSCRIPT!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				CmMod.LOGGER.warn("Failed to load dependency y for procedure MachineBlockAnimationSCRIPT!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				CmMod.LOGGER.warn("Failed to load dependency z for procedure MachineBlockAnimationSCRIPT!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				CmMod.LOGGER.warn("Failed to load dependency world for procedure MachineBlockAnimationSCRIPT!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		if (((world.getBlockState(new BlockPos((int) x, (int) y, (int) z))).getBlock() == MachinePressBlock.block.getDefaultState().getBlock())) {
+			{
+				BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+				BlockState _bs = MachinePressAnimation1Block.block.getDefaultState();
+				BlockState _bso = world.getBlockState(_bp);
+				for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+					Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+					if (_property != null && _bs.get(_property) != null)
+						try {
+							_bs = _bs.with(_property, (Comparable) entry.getValue());
+						} catch (Exception e) {
+						}
+				}
+				TileEntity _te = world.getTileEntity(_bp);
+				CompoundNBT _bnbt = null;
+				if (_te != null) {
+					_bnbt = _te.write(new CompoundNBT());
+					_te.remove();
+				}
+				world.setBlockState(_bp, _bs, 3);
+				if (_bnbt != null) {
+					_te = world.getTileEntity(_bp);
+					if (_te != null) {
+						try {
+							_te.read(_bso, _bnbt);
+						} catch (Exception ignored) {
+						}
+					}
+				}
+			}
+			if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+				((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("1"), (false));
+			}
+			new Object() {
+				private int ticks = 0;
+				private float waitTicks;
+				private IWorld world;
+				public void start(IWorld world, int waitTicks) {
+					this.waitTicks = waitTicks;
+					MinecraftForge.EVENT_BUS.register(this);
+					this.world = world;
+				}
+
+				@SubscribeEvent
+				public void tick(TickEvent.ServerTickEvent event) {
+					if (event.phase == TickEvent.Phase.END) {
+						this.ticks += 1;
+						if (this.ticks >= this.waitTicks)
+							run();
+					}
+				}
+
+				private void run() {
+					{
+						BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+						BlockState _bs = MachinePressAnimation2Block.block.getDefaultState();
+						BlockState _bso = world.getBlockState(_bp);
+						for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+							Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+							if (_property != null && _bs.get(_property) != null)
+								try {
+									_bs = _bs.with(_property, (Comparable) entry.getValue());
+								} catch (Exception e) {
+								}
+						}
+						TileEntity _te = world.getTileEntity(_bp);
+						CompoundNBT _bnbt = null;
+						if (_te != null) {
+							_bnbt = _te.write(new CompoundNBT());
+							_te.remove();
+						}
+						world.setBlockState(_bp, _bs, 3);
+						if (_bnbt != null) {
+							_te = world.getTileEntity(_bp);
+							if (_te != null) {
+								try {
+									_te.read(_bso, _bnbt);
+								} catch (Exception ignored) {
+								}
+							}
+						}
+					}
+					if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+						((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("2"), (false));
+					}
+					new Object() {
+						private int ticks = 0;
+						private float waitTicks;
+						private IWorld world;
+						public void start(IWorld world, int waitTicks) {
+							this.waitTicks = waitTicks;
+							MinecraftForge.EVENT_BUS.register(this);
+							this.world = world;
+						}
+
+						@SubscribeEvent
+						public void tick(TickEvent.ServerTickEvent event) {
+							if (event.phase == TickEvent.Phase.END) {
+								this.ticks += 1;
+								if (this.ticks >= this.waitTicks)
+									run();
+							}
+						}
+
+						private void run() {
+							{
+								BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+								BlockState _bs = MachinePressAnimation3Block.block.getDefaultState();
+								BlockState _bso = world.getBlockState(_bp);
+								for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+									Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+									if (_property != null && _bs.get(_property) != null)
+										try {
+											_bs = _bs.with(_property, (Comparable) entry.getValue());
+										} catch (Exception e) {
+										}
+								}
+								TileEntity _te = world.getTileEntity(_bp);
+								CompoundNBT _bnbt = null;
+								if (_te != null) {
+									_bnbt = _te.write(new CompoundNBT());
+									_te.remove();
+								}
+								world.setBlockState(_bp, _bs, 3);
+								if (_bnbt != null) {
+									_te = world.getTileEntity(_bp);
+									if (_te != null) {
+										try {
+											_te.read(_bso, _bnbt);
+										} catch (Exception ignored) {
+										}
+									}
+								}
+							}
+							if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+								((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("3"), (false));
+							}
+							new Object() {
+								private int ticks = 0;
+								private float waitTicks;
+								private IWorld world;
+								public void start(IWorld world, int waitTicks) {
+									this.waitTicks = waitTicks;
+									MinecraftForge.EVENT_BUS.register(this);
+									this.world = world;
+								}
+
+								@SubscribeEvent
+								public void tick(TickEvent.ServerTickEvent event) {
+									if (event.phase == TickEvent.Phase.END) {
+										this.ticks += 1;
+										if (this.ticks >= this.waitTicks)
+											run();
+									}
+								}
+
+								private void run() {
+									{
+										BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+										BlockState _bs = MachinePressAnimation2Block.block.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										TileEntity _te = world.getTileEntity(_bp);
+										CompoundNBT _bnbt = null;
+										if (_te != null) {
+											_bnbt = _te.write(new CompoundNBT());
+											_te.remove();
+										}
+										world.setBlockState(_bp, _bs, 3);
+										if (_bnbt != null) {
+											_te = world.getTileEntity(_bp);
+											if (_te != null) {
+												try {
+													_te.read(_bso, _bnbt);
+												} catch (Exception ignored) {
+												}
+											}
+										}
+									}
+									if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+										((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("4"), (false));
+									}
+									MinecraftForge.EVENT_BUS.unregister(this);
+								}
+							}.start(world, (int) 20);
+							new Object() {
+								private int ticks = 0;
+								private float waitTicks;
+								private IWorld world;
+								public void start(IWorld world, int waitTicks) {
+									this.waitTicks = waitTicks;
+									MinecraftForge.EVENT_BUS.register(this);
+									this.world = world;
+								}
+
+								@SubscribeEvent
+								public void tick(TickEvent.ServerTickEvent event) {
+									if (event.phase == TickEvent.Phase.END) {
+										this.ticks += 1;
+										if (this.ticks >= this.waitTicks)
+											run();
+									}
+								}
+
+								private void run() {
+									{
+										BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+										BlockState _bs = MachinePressAnimation1Block.block.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										TileEntity _te = world.getTileEntity(_bp);
+										CompoundNBT _bnbt = null;
+										if (_te != null) {
+											_bnbt = _te.write(new CompoundNBT());
+											_te.remove();
+										}
+										world.setBlockState(_bp, _bs, 3);
+										if (_bnbt != null) {
+											_te = world.getTileEntity(_bp);
+											if (_te != null) {
+												try {
+													_te.read(_bso, _bnbt);
+												} catch (Exception ignored) {
+												}
+											}
+										}
+									}
+									if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+										((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("5"), (false));
+									}
+									MinecraftForge.EVENT_BUS.unregister(this);
+								}
+							}.start(world, (int) 20);
+							new Object() {
+								private int ticks = 0;
+								private float waitTicks;
+								private IWorld world;
+								public void start(IWorld world, int waitTicks) {
+									this.waitTicks = waitTicks;
+									MinecraftForge.EVENT_BUS.register(this);
+									this.world = world;
+								}
+
+								@SubscribeEvent
+								public void tick(TickEvent.ServerTickEvent event) {
+									if (event.phase == TickEvent.Phase.END) {
+										this.ticks += 1;
+										if (this.ticks >= this.waitTicks)
+											run();
+									}
+								}
+
+								private void run() {
+									{
+										BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+										BlockState _bs = MachinePressBlock.block.getDefaultState();
+										BlockState _bso = world.getBlockState(_bp);
+										for (Map.Entry<Property<?>, Comparable<?>> entry : _bso.getValues().entrySet()) {
+											Property _property = _bs.getBlock().getStateContainer().getProperty(entry.getKey().getName());
+											if (_property != null && _bs.get(_property) != null)
+												try {
+													_bs = _bs.with(_property, (Comparable) entry.getValue());
+												} catch (Exception e) {
+												}
+										}
+										TileEntity _te = world.getTileEntity(_bp);
+										CompoundNBT _bnbt = null;
+										if (_te != null) {
+											_bnbt = _te.write(new CompoundNBT());
+											_te.remove();
+										}
+										world.setBlockState(_bp, _bs, 3);
+										if (_bnbt != null) {
+											_te = world.getTileEntity(_bp);
+											if (_te != null) {
+												try {
+													_te.read(_bso, _bnbt);
+												} catch (Exception ignored) {
+												}
+											}
+										}
+									}
+									if (entity instanceof PlayerEntity && !entity.world.isRemote()) {
+										((PlayerEntity) entity).sendStatusMessage(new StringTextComponent("6"), (false));
+									}
+									MinecraftForge.EVENT_BUS.unregister(this);
+								}
+							}.start(world, (int) 20);
+							MinecraftForge.EVENT_BUS.unregister(this);
+						}
+					}.start(world, (int) 20);
+					MinecraftForge.EVENT_BUS.unregister(this);
+				}
+			}.start(world, (int) 20);
+		}
+	}
+}
